27f5e0f342485463c995c3a1455e9ffed47a501e
Victor Jonsson <kontakt@victorjonsson.se>
2017-03-14 22:17:07 +0100

    reformatted code

--- language/language.go
+++ language/language.go
@@ -3,72 +3,71 @@ package language;
 import "time"
 
 type Dialogs interface {
-	Now() string
-	Recently() string
-	Soon() string
-	InXSeconds() string
-	XSecondsAgo() string
-	InOneMinute() string
-	OneMinuteAgo() string
-	InXMinutes() string
-	XMinutesAgo() string
-	FormatTodayDate(t time.Time) string
-	FormatYesterdayDate(t time.Time) string
-	FormatTomorrowDate(t time.Time) string
-	FormatDefaultDateFormat(t time.Time) string
+    Now() string
+    Recently() string
+    Soon() string
+    InXSeconds() string
+    XSecondsAgo() string
+    InOneMinute() string
+    OneMinuteAgo() string
+    InXMinutes() string
+    XMinutesAgo() string
+    FormatTodayDate(t time.Time) string
+    FormatYesterdayDate(t time.Time) string
+    FormatTomorrowDate(t time.Time) string
+    FormatDefaultDateFormat(t time.Time) string
 }
 
 type EnglishDialogs struct {
 }
 
 func (EnglishDialogs) Now() string {
-	return "Right now";
+    return "Right now";
 }
 
 func (EnglishDialogs) Recently() string {
-	return "Recently"
+    return "Recently"
 }
 
 func (EnglishDialogs) Soon() string {
-	return "Soon"
+    return "Soon"
 }
 
 func (EnglishDialogs) InXSeconds() string {
-	return "In about %s seconds"
+    return "In about %s seconds"
 }
 
 func (EnglishDialogs) XSecondsAgo() string {
-	return "About %s seconds ago"
+    return "About %s seconds ago"
 }
 
 func (EnglishDialogs) InXMinutes() string {
-	return "In about %s minutes"
+    return "In about %s minutes"
 }
 
 func (EnglishDialogs) InOneMinute() string {
-	return "In about 1 minute"
+    return "In about 1 minute"
 }
 func (EnglishDialogs) OneMinuteAgo() string {
-	return "About 1 minute ago"
+    return "About 1 minute ago"
 }
 
 func (EnglishDialogs) XMinutesAgo() string {
-	return "About %s minutes ago"
+    return "About %s minutes ago"
 }
 
 func (EnglishDialogs) FormatTodayDate(t time.Time) string {
-	return t.Format("Today 3:04PM")
+    return t.Format("Today 3:04PM")
 }
 
 func (EnglishDialogs) FormatYesterdayDate(t time.Time) string {
-	return t.Format("Yesterday 3:04PM")
+    return t.Format("Yesterday 3:04PM")
 }
 
 func (EnglishDialogs) FormatTomorrowDate(t time.Time) string {
-	return t.Format("Tomorrow 3:04PM")
+    return t.Format("Tomorrow 3:04PM")
 }
 
 func (EnglishDialogs) FormatDefaultDateFormat(t time.Time) string {
-	return t.Format("Mon Jan 2 15:04:05")
+    return t.Format("Mon Jan 2 15:04:05")
 }
-
--- timeagoo_test.go
+++ timeagoo_test.go
@@ -1,44 +1,44 @@
 package timeagoo
 
 import (
-	"testing"
-	"timeagoo/language"
-	"time"
-	"fmt"
+    "testing"
+    "timeagoo/language"
+    "time"
+    "fmt"
 )
 
 func TestFormat(t *testing.T) {
-	english := language.EnglishDialogs{}
-	cases := map[time.Time]string {
-		getTime(0): english.Now(),
+    english := language.EnglishDialogs{}
+    cases := map[time.Time]string{
+        getTime(0): english.Now(),
 
-		getTime(9): english.Soon(),
-		getTime(10): fmt.Sprintf(english.InXSeconds(), "10"),
-		getTime(60): fmt.Sprintf(english.InXSeconds(), "60"),
-		getTime(61): english.InOneMinute(),
-		getTime(119): fmt.Sprintf(english.InXMinutes(), "2"),
-		getTime(3600): english.FormatTodayDate(getTime(3600)),
-		getTime(86400): english.FormatTomorrowDate(getTime(86400)),
-		getTime(8640000): english.FormatDefaultDateFormat(getTime(8640000)),
+        getTime(9):       english.Soon(),
+        getTime(10):      fmt.Sprintf(english.InXSeconds(), "10"),
+        getTime(60):      fmt.Sprintf(english.InXSeconds(), "60"),
+        getTime(61):      english.InOneMinute(),
+        getTime(119):     fmt.Sprintf(english.InXMinutes(), "2"),
+        getTime(3600):    english.FormatTodayDate(getTime(3600)),
+        getTime(86400):   english.FormatTomorrowDate(getTime(86400)),
+        getTime(8640000): english.FormatDefaultDateFormat(getTime(8640000)),
 
-		getTime(-9): english.Recently(),
-		getTime(-10): fmt.Sprintf(english.XSecondsAgo(), "10"),
-		getTime(-60): fmt.Sprintf(english.XSecondsAgo(), "60"),
-		getTime(-61): english.OneMinuteAgo(),
-		getTime(-119): fmt.Sprintf(english.XMinutesAgo(), "2"),
-		getTime(-3600): english.FormatTodayDate(getTime(-3600)),
-		getTime(-86400): english.FormatYesterdayDate(getTime(-86400)),
-		getTime(-8640000): english.FormatDefaultDateFormat(getTime(-8640000)),
-	}
+        getTime(-9):       english.Recently(),
+        getTime(-10):      fmt.Sprintf(english.XSecondsAgo(), "10"),
+        getTime(-60):      fmt.Sprintf(english.XSecondsAgo(), "60"),
+        getTime(-61):      english.OneMinuteAgo(),
+        getTime(-119):     fmt.Sprintf(english.XMinutesAgo(), "2"),
+        getTime(-3600):    english.FormatTodayDate(getTime(-3600)),
+        getTime(-86400):   english.FormatYesterdayDate(getTime(-86400)),
+        getTime(-8640000): english.FormatDefaultDateFormat(getTime(-8640000)),
+    }
 
-	for in, want := range cases {
-		got := Format(in, english)
-		if got != want {
-			t.Errorf("Format(%q, english) == %q, want %q", in, got, want)
-		}
-	}
+    for in, want := range cases {
+        got := Format(in, english)
+        if got != want {
+            t.Errorf("Format(%q, english) == %q, want %q", in, got, want)
+        }
+    }
 }
 
 func getTime(secondsAgo int64) time.Time {
-	return time.Unix(time.Now().Unix() +secondsAgo, 0)
+    return time.Unix(time.Now().Unix()+secondsAgo, 0)
 }
